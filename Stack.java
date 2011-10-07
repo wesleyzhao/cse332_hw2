11f53a0f9215464a8077eaf9158aec1e3e545c9f
Wesley Zhao <wesley.zhao@gmail.com>
2011-10-06 22:53:12 -0700

    Made Stacks more efficient again

--- Stack.java
+++ Stack.java
@@ -2,12 +2,10 @@
 public class Stack {
 	private int[] mainStack;
 	private int currentIndex;
-	private int stackSize;
 	
 	public Stack(){
-		mainStack = new int[1000];
+		mainStack = new int[1000];	//assume stack size is 1000
 		currentIndex = 0;
-		stackSize = 0;
 	}
 	
 	public int pop(){
@@ -16,21 +14,8 @@ public class Stack {
 	 * which is the last integer to be added
 	 * also removed that integer from the stack
 	 */
-		/*
-		int top = (Integer)mainStack[0]; //will throw an error if the stack is empty...
-		int curIndex = 1;
-		while (mainStack[curIndex] != null){
-			mainStack[curIndex-1] = mainStack[curIndex];
-			curIndex++;
-		}
-		mainStack[curIndex-1] = null; //removes the redundant element at the bottom of the array
-		return top;
-		*/
-		int temp = mainStack[currentIndex];
-		stackSize --;
-		if (currentIndex == 0 )currentIndex = mainStack.length -1;
-		else currentIndex--;
-		return temp;
+		currentIndex--;
+		return mainStack[currentIndex+1];
 	}
 	
 	public int push(int number){
@@ -38,22 +23,8 @@ public class Stack {
 	 * movesnumber int number to the top of the stack
 	 * returns the arg number
 	 */
-		/*
-		int curIndex = 0;
-		Object temp = mainStack[curIndex];
-		while (mainStack[curIndex+1] != null){
-			temp = mainStack[curIndex+1];
-			mainStack[curIndex+1] = mainStack[curIndex];
-			curIndex++;
-		}
-		mainStack[curIndex+1] = temp;
-		mainStack[0] = (Object)number;
-		return number;
-		*/
-		if(currentIndex >= (mainStack.length-1)) currentIndex = 0;
-		else currentIndex++;
+		currentIndex++;
 		mainStack[currentIndex] = number;
-		stackSize++;
 		return number;
 	}
 	
@@ -66,7 +37,7 @@ public class Stack {
 	}
 	
 	public boolean isEmpty(){
-		if (stackSize == 0){
+		if (currentIndex == 0){
 			return true;
 		}
 		else{
